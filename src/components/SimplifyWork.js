56418aeabeef83091e747c5a15aea780120249a9
Muhammad Alyan <alyan0332@gmail.com>
2022-09-05 21:47:47 +0500

    make simpility work into seperate component

---
+++ src/components/SimplifyWork.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SimplifyWork = () => {
+  return (
+    <section className="mt-8 bg-orange-500 py-16 px-8 md:flex md:items-center md:justify-around">
+      <h2 className="mx-auto my-6 w-fit text-center text-3xl font-bold text-white md:mx-0 md:max-w-[500px] md:text-left md:text-4xl">
+        Simplify how your team works today.
+      </h2>
+      <button className="button mx-auto bg-white text-orange-500 hover:bg-slate-200 md:mx-0 md:h-12">
+        Get Started
+      </button>
+    </section>
+  );
+};
+
+export default SimplifyWork;

35f582c031ad5c228d80cb88c98bb99af884b89b
Muhammad Alyan <alyan0332@gmail.com>
2022-09-06 10:45:19 +0500

    add testimonails and features data

---
+++ src/data.js
@@ -0,0 +1,56 @@
+import anishaAvatar from '../src/assets/images/avatar-anisha.png';
+import aliAvatar from '../src/assets/images/avatar-ali.png';
+import richardAvatar from '../src/assets/images/avatar-richard.png';
+import shanaiAvatar from '../src/assets/images/avatar-shanai.png';
+
+export const featuresData = [
+  {
+    id: '01',
+    title: 'Track company-wide progress',
+    description:
+      'See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again.',
+  },
+  {
+    id: '02',
+    title: 'Advanced built-in reports',
+    description:
+      'Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed.',
+  },
+  {
+    id: '03',
+    title: 'Everything you need in one place',
+    description:
+      'Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.',
+  },
+];
+
+export const testimonialData = [
+  {
+    id: 0,
+    image: anishaAvatar,
+    name: 'Anisha Li',
+    review:
+      "“Manage has supercharged our team's workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”",
+  },
+  {
+    id: 1,
+    image: aliAvatar,
+    name: 'Ali Bravo',
+    review:
+      '“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”',
+  },
+  {
+    id: 2,
+    image: richardAvatar,
+    name: 'Richard Watts',
+    review:
+      "“Manage allows us to provide structure and process. It keeps us organized and focused. I can't stop recommending them to everyone I talk to!”",
+  },
+  {
+    id: 3,
+    image: shanaiAvatar,
+    name: 'Shanai Gough',
+    review:
+      '“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”',
+  },
+];
